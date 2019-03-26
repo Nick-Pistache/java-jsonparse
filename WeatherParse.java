4d08309e15b43531ada8c15fef5ac0b886b982a2
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-03-26 18:45:37 +0100

    JSON

--- WeatherParse.java
+++ WeatherParse.java
@@ -25,15 +25,35 @@ public class WeatherParse {
         }
         
         // TODO parser le fichier
+        JSONParser parser = new JSONParser();
+        Object jsonParsed = null;
+        try {
+        jsonParsed = parser.parse(jsonFile);
+        } catch (ParseException | IOException e) {
+        e.printStackTrace();
+        }
 
         // TODO récupérer la racine du document
+        JSONObject root = (JSONObject) jsonParsed;
 
         // TODO afficher la valeur de l'attribut "name" de la racine
-
+        String name = (String) root.get("name");
+        System.out.println(name);
         // TODO afficher les valeurs des attributs "lat" et "lon" de l'élément "coord" contenu dans la racine
+        JSONObject coord = (JSONObject) root.get("coord");
+        double lat = (double) coord.get("lat");
+        double lon = (double) coord.get("lon");
+        System.out.println(lat);
+        System.out.println(lon);       
 
         // TODO parcourir tous les éléments de "weather" et afficher le contenu de "main"
+        JSONArray weather = (JSONArray) root.get("weather");
 
+        for (int i = 0 ; i < weather.size() ; i++) {
+            JSONObject value = (JSONObject) weather.get(i);
+            String main = (String) value.get("main");
+            System.out.println(main);           
+        }
         /*
             Résultat attendu :
             * City name: London
@@ -42,5 +62,6 @@ public class WeatherParse {
             * Weather: Drizzle
             * Weather: Clear
         */
+        
     }
 }
